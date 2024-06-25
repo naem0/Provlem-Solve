ff89a7ffd8e814359d63945783389bfdd8644171
MD Naem <mmnayeem408@gmail.com>
2024-06-25 13:23:12 +0600

    length Of Longest Substring

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var lengthOfLongestSubstring = function (s) {
+    let sArray =[]
+    for (let i = 0; i < s.length; i++) {
+        sArray.push(s[i]);
+        sArray.find((element) => {
+            if (element != s[i]) {
+                
+            }
+        });
+    };
+    return sArray
+};
+// console.log(lengthOfLongestSubstring('hellllo'))
+
