cecf1eddc6aea58c32cc6559bb6dbda30649e284
MD Naem <mmnayeem408@gmail.com>
2024-06-25 13:23:35 +0600

    length Of Longest Substring

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ var lengthOfLongestSubstring = function (s) {
 };
 // console.log(lengthOfLongestSubstring('hellllo'))
 
+// 4. Median of Two Sorted Arrays
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    const newArray = [...nums1,...nums2].sort(function(a, b){return a-b});
+    console.log(newArray)
+    const n = newArray.length;
+    if (n%2) {
+        const m =Math.floor(n/2);
+        return (newArray[m]);
+    }else{
+        const midal = newArray[(n/2)-1]+newArray[(n/2)];
+        return midal/2;
+    }
+};
+// console.log(findMedianSortedArrays([3], [-2,-1]))
